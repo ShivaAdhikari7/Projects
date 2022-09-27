cea2485fa2cb1adbd5a72e4d5916028df04c5b8a
Shiva Adhikari <shivaadhikari499@gmail.com>
2022-09-27 08:44:51 +0545

    logic for accordain is complete

---
+++ CSS/Components/01.Accordian/script.js
@@ -0,0 +1,14 @@
+let first = document.getElementById("first");
+let second = document.getElementById("second");
+let third = document.getElementById("third");
+let item = document.getElementsByClassName("item");
+
+first.addEventListener("click", () => {
+  item[0].classList.toggle("open");
+});
+second.addEventListener("click", () => {
+  item[1].classList.toggle("open");
+});
+third.addEventListener("click", () => {
+  item[2].classList.toggle("open");
+});
