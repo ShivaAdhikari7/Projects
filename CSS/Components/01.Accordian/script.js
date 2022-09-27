2a2b41c795731f7532b6d6de576d2c0a4bcad5bc
Shiva Adhikari <shivaadhikari499@gmail.com>
2022-09-27 09:29:56 +0545

    complete accordian

--- CSS/Components/01.Accordian/script.js
+++ CSS/Components/01.Accordian/script.js
@@ -2,7 +2,7 @@ let first = document.getElementById("first");
 let second = document.getElementById("second");
 let third = document.getElementById("third");
 let item = document.getElementsByClassName("item");
-
+item[1].classList.add("open");
 first.addEventListener("click", () => {
   item[0].classList.toggle("open");
 });
