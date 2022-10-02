6d30d95ca297653ae481e5838448a748977d8bad
Shiva Adhikari <shivaadhikari499@gmail.com>
2022-10-02 21:27:59 +0545

    functionality of this keyword complete

--- JavaScript/How JS work Behind the secene/script.js
+++ JavaScript/How JS work Behind the secene/script.js
@@ -1,23 +1,31 @@
-// Primitive variable vs Refrence variable:
+// // Primitive variable vs Refrence variable:
+// // var firstName = "siwani";
+// // This keyword in method:
+// let girl = {
+//   firstName: "Savya",
+//   lastName: "Gaihre",
+//   birthYear: 1998,
 
-// This keyword in method:
-let girl = {
-  firstName: "Savya",
-  lastName: "Gaihre",
-  birthYear: 1998,
+//   calcAge: function calcAge() {
+//     console.log(this);
+//     havegotra = true;
 
-  calcAge: function calcAge() {
-    console.log(this);
-    return 2022 - this.birthYear;
-  },
-};
+//     let self = this;
+//     const func = function () {
+//       if (havegotra) console.log(`${self.firstName} is  a brahmin`);
+//     };
+//     func();
 
-let girl2 = {
-  firstName: "Siwani",
-  lastName: "Adhikari",
-  birthYear: 1992,
-};
+//     return 2022 - this.birthYear;
+//   },
+// };
 
-girl2.calcAge = girl.calcAge;
+// let girl2 = {
+//   firstName: "Siwani",
+//   lastName: "Adhikari",
+//   birthYear: 1992,
+// };
 
-girl2.calcAge();
+// girl2.calcAge = girl.calcAge;
+
+// girl2.calcAge();
