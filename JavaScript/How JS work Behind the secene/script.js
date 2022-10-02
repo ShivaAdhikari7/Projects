439d16ec71b09fea50e1286363c8bc4167384fde
Shiva Adhikari <shivaadhikari499@gmail.com>
2022-10-02 21:39:57 +0545

    Refrence vs Primitive data exercise complete

--- JavaScript/How JS work Behind the secene/script.js
+++ JavaScript/How JS work Behind the secene/script.js
@@ -32,11 +32,32 @@
 
 // Arguments: We don't have arguments in arrow function but do have in normal function:
 
-let a = function (a, b) {
-  console.log(arguments);
-  for (let key in arguments) {
-    console.log(key);
-  }
-  return a + b;
+// let a = function (a, b) {
+//   console.log(arguments);
+//   for (let key in arguments) {
+//     console.log(key);
+//   }
+//   return a + b;
+// };
+// a(3, 4, 5, 6);
+
+// Primitive vs Refrence variables:
+
+let age = 10;
+let oldAge = 50;
+age = 40;
+console.log(age); //The value is changed only for the age not for the old age;
+
+let girl = {
+  firstName: "Siwani",
+  lastName: "Adhikari",
+  age: 27,
 };
-a(3, 4, 5, 6);
+
+let marriedGirl = Object.assign({}, girl);
+// let marriedGirl = {};
+// marriedGirl = girl;
+
+marriedGirl.lastName = "Gaihre";
+console.log(marriedGirl);
+console.log(girl);
