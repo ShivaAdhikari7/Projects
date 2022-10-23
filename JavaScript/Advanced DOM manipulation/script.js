683491b94451fb86a3b087f47f4aa1a91c4341d0
Shiva Adhikari <79002274+ShivaAdhikari7@users.noreply.github.com>
2022-10-23 12:10:30 +0545

    dom manipulation complete

--- JavaScript/Advanced DOM manipulation/script.js
+++ JavaScript/Advanced DOM manipulation/script.js
@@ -74,32 +74,31 @@ btnScroll.addEventListener('click', () => {
 
 // Operations section:
 
-// tabsContainer.addEventListener('click', function (e) {
-//   let clicked = e.target.closest('.operations__tab');
+tabsContainer.addEventListener('click', function (e) {
+  let clicked = e.target.closest('.operations__tab');
 
-//   if (!clicked) return;
+  if (!clicked) return;
 
-//   // Remove active classes from both button and the content
-//   tabBtn.forEach(b => {
-//     b.classList.remove('operations__tab--active');
-//   });
-//   // Remove active class from content:
-//   tabsContent.forEach(t => {
-//     t.classList.remove('operations__content--active');
-//   });
-//   // Add active to the button:
-//   clicked.classList.add('operations__tab--active');
+  // Remove active classes from both button and the content
+  tabBtn.forEach(b => {
+    b.classList.remove('operations__tab--active');
+  });
+  // Remove active class from content:
+  tabsContent.forEach(t => {
+    t.classList.remove('operations__content--active');
+  });
+  // Add active to the button:
+  clicked.classList.add('operations__tab--active');
 
-//   document
-//     .querySelector(`.operations__content--${clicked.dataset.tab}`)
-//     .classList.add('operations__content--active');
-// });
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
 
-// console.log(secCords.top);
-// window.addEventListener('scroll', () => {
-//   if (window.scrollY > secCords.top) nav.classList.add('sticky');
-//   else nav.classList.remove('sticky');
-// });
+window.addEventListener('scroll', () => {
+  if (window.scrollY > secCords.top) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+});
 
 // Passing arguments to the eventHandler:
 
@@ -121,3 +120,100 @@ const setOpacity = function (e) {
 nav.addEventListener('mouseover', setOpacity.bind('0.5'));
 
 nav.addEventListener('mouseout', setOpacity.bind('1'));
+
+// Adding the sticky navbar using the intersection observer API:
+
+const header = document.querySelector('header');
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+headerObserver.observe(header);
+
+// Another example of API intersectiong:
+const allSections = document.querySelectorAll('.section');
+
+const sectionObs = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+const sectionObserver = new IntersectionObserver(sectionObs, {});
+
+allSections.forEach(section => {
+  // section.classList.add('section--hidden');
+  sectionObserver.observe(section);
+});
+
+// Lazy loading of images:
+
+const allImages = document.querySelectorAll('img[data-src]');
+
+const lazyLoadImage = function (entries, observer) {
+  const [entry] = entries;
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener('load', () => {
+    entry.target.classList.remove('lazy-img');
+  });
+  observer.unobserve(entry.target);
+};
+
+const imageObserver = new IntersectionObserver(lazyLoadImage, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+});
+
+allImages.forEach(img => {
+  img.classList.add('lazy-img');
+  imageObserver.observe(img);
+});
+
+// Slider:
+const allSlides = document.querySelectorAll('.slide');
+const slider = document.querySelector('.slider');
+const leftBtn = document.querySelector('.slider__btn--left');
+const rightBtn = document.querySelector('.slider__btn--right');
+
+const maxLength = allSlides.length;
+let curSlide = 0;
+
+// slider.style.transform = 'scale(0.4) translateX(-800px)';
+
+const goToSlide = function (slide) {
+  allSlides.forEach((img, i) => {
+    img.style.transform = `translateX(${100 * (i - curSlide)}%)`;
+  });
+};
+
+const nextSlide = function () {
+  if (curSlide < maxLength - 1) {
+    curSlide++;
+  } else {
+    curSlide = 0;
+  }
+  goToSlide();
+};
+
+const prevSlide = function () {
+  if (curSlide === 0) {
+    curSlide = maxLength - 1;
+  } else {
+    curSlide--;
+  }
+  goToSlide();
+};
+goToSlide(0);
+
+rightBtn.addEventListener('click', nextSlide);
+
+leftBtn.addEventListener('click', prevSlide);
