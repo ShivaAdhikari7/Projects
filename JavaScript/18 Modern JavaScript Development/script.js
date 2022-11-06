f8a81dd0a85c526acce18a3e0e5d84ed3da74659
Shiva Adhikari <79002274+ShivaAdhikari7@users.noreply.github.com>
2022-11-06 18:18:42 +0545

    learning about npm

--- JavaScript/18 Modern JavaScript Development/script.js
+++ JavaScript/18 Modern JavaScript Development/script.js
@@ -1,5 +1,7 @@
 // import { arr as a } from "./shoppingCart.js";
 
+// import { cart } from "./shoppingCart";
+
 // console.log("Importing module");
 // console.log(a);
 // import { cart, arr } from "./shoppingCart.js";
@@ -24,3 +26,48 @@
 // //   .then((data) => console.log(data))
 // //   .catch((err) => console.error(err));
 // console.log("Something");
+
+// const shoppingCart = (function () {
+//   const cart = [];
+//   const shippingCost = 100;
+//   const total = 230;
+
+//   const addToCart = function (item, quantity) {
+//     cart.push({ quantity, item });
+//     console.log(
+//       `${item} having ${quantity} and shipping cost is ${shippingCost}`
+//     );
+//   };
+//   const placeOrder = function (item, quantity) {
+//     console.log(`${item} with ${quantity} is ordered`);
+//   };
+
+//   return {
+//     cart,
+//     addToCart,
+//     placeOrder,
+//   };
+// })();
+
+// shoppingCart.addToCart("apple", 2);
+// console.log(shoppingCart);
+
+// Using the npm and node modules:
+// import cloneDeep from "./node_modules/lodash-es/cloneDeep.js";
+
+// const object1 = {
+//   fullName: "Shiva Adhikari",
+//   cart: [
+//     { quantity: 5, item: "apple" },
+//     { quantity: 5, item: "pizza" },
+//   ],
+//   user: { loggedIn: false },
+// };
+
+// const copiedObject = Object.assign({}, object1);
+// const cloneDeepObject = cloneDeep(object1);
+// object1.user.loggedIn = true;
+
+// console.log(object1);
+// console.log(copiedObject);
+// console.log(cloneDeepObject);
