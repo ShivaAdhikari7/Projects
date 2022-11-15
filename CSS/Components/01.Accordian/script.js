7acc4d112890ac7138c23a8b88ac81a74f726282
Shiva Adhikari <79002274+ShivaAdhikari7@users.noreply.github.com>
2022-11-15 08:40:52 +0545

    setup for forkify application again

--- CSS/Components/01.Accordian/script.js
+++ CSS/Components/01.Accordian/script.js
@@ -1,14 +1,21 @@
 let first = document.getElementById("first");
 let second = document.getElementById("second");
 let third = document.getElementById("third");
-let item = document.getElementsByClassName("item");
-item[1].classList.add("open");
-first.addEventListener("click", () => {
-  item[0].classList.toggle("open");
-});
-second.addEventListener("click", () => {
-  item[1].classList.toggle("open");
-});
-third.addEventListener("click", () => {
-  item[2].classList.toggle("open");
-});
+let items = document.getElementsByClassName("item");
+
+for (const item of items) {
+  item.addEventListener("click", (e) => {
+    const click = e.target.closest(".item");
+    click.classList.toggle("open");
+  });
+}
+// item[1].classList.add("open");
+// first.addEventListener("click", () => {
+//   item[0].classList.toggle("open");
+// });
+// second.addEventListener("click", () => {
+//   item[1].classList.toggle("open");
+// });
+// third.addEventListener("click", () => {
+//   item[2].classList.toggle("open");
+// });
