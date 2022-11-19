53044bb763e99ac90db05ca9140afc94d3f4d799
Shiva Adhikari <79002274+ShivaAdhikari7@users.noreply.github.com>
2022-11-19 22:20:21 +0545

    pagination complete

--- Node.js/03. Natours/starter/controllers/tourControllers.js
+++ Node.js/03. Natours/starter/controllers/tourControllers.js
@@ -2,16 +2,54 @@ const Tour = require("./../models/tourModel");
 
 const getAllTours = async (req, res) => {
   try {
+    //1A) Filtering:
     const queryObj = { ...req.query };
 
-    excludedFields = ["page", "sort", "limit"];
+    const excludeFields = ["sort", "page", "fields", "limit"];
 
-    excludedFields.forEach((el) => {
-      delete queryObj[el];
-    });
+    excludeFields.forEach((el) => delete queryObj[el]);
 
-    const query = Tour.find(queryObj);
+    // 1B) Advanced Filtering:
+    console.log(queryObj);
+    let queryStr = JSON.stringify(queryObj);
+
+    queryStr = queryStr.replace(/\b(gt|lt|gte|lte)\b/g, (match) => `$${match}`);
+
+    const filteredQuery = JSON.parse(queryStr);
+    let query = Tour.find(filteredQuery);
+
+    console.log(req.query);
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join("");
+
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort("-createdAt");
+    }
+
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      console.log(fields);
+      query = query.select(fields);
+    } else {
+      query = query.select("-__v");
+    }
+
+    // Pagination:
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error("Page does not exist");
+    }
+
     const tours = await query;
+
     res.status(200).json({
       status: "success",
       data: {
@@ -22,7 +60,7 @@ const getAllTours = async (req, res) => {
   } catch (err) {
     res.status(200).json({
       status: "fail",
-      message: err,
+      message: err.message,
     });
   }
 };
